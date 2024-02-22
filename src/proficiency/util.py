12cc2e9c0731ecf1d4b50384990ea07e9b9dc5cf
xxyzz <gitpull@protonmail.com>
2024-02-22 19:26:45 +0800

    Split semicolon first then split comma
    Only keep the text before the first comma, texts after comma usually
    are not a complete sentence.

--- src/proficiency/util.py
+++ src/proficiency/util.py
@@ -39,13 +39,14 @@ def get_short_def(gloss: str, gloss_lang: str) -> str:
         "",
         gloss,
     )
-    gloss = min(re.split(";|；|,", gloss), key=len)
+    gloss = min(re.split(";|；", gloss), key=len)
+    gloss = re.split(r",|，", gloss, 1)[0]
     gloss = min(gloss.split("/"), key=len)
     if gloss_lang == "zh":
         gloss = min(gloss.split("、"), key=len)
     if gloss_lang == "es" and "|" in gloss:
         gloss = gloss.split("|", 1)[1]
-    return gloss.strip()
+    return remove_full_stop(gloss.strip())
 
 
 def load_difficulty_data(lemma_lang: str) -> dict[str, int]:
--- tests/test_util.py
+++ tests/test_util.py
@@ -1,8 +1,38 @@
 from unittest import TestCase
 
+from proficiency.util import get_short_def
+
 
 class TestUtil(TestCase):
     def test_nested_parentheses(self) -> None:
-        from proficiency.util import remove_parentheses
+        # https://en.wiktionary.org/wiki/cadet_house
+        self.assertEqual(
+            get_short_def(
+                "Synonym of cadet branch (“house (dynasty) descended from one of "
+                "the patriarch's younger sons”)",
+                "en",
+            ),
+            "Synonym of cadet branch",
+        )
 
-        self.assertEqual(remove_parentheses("a (b (c)) d"), "a d")
+    def test_get_short_def_stop(self) -> None:
+        # https://en.wiktionary.org/wiki/jack_jumper
+        self.assertEqual(
+            get_short_def(
+                "Any of various small species of ant of the genus Myrmecia, "
+                "often capable of jumping and having a painful sting; a jumper. "
+                "(Also used attributively.)",
+                "en",
+            ),
+            "a jumper",
+        )
+
+    def test_mastodonian(self) -> None:
+        # https://en.wiktionary.org/wiki/mastodonian
+        self.assertEqual(
+            get_short_def(
+                "Of, related to, or characteristic of a mastodon; large; powerful.",
+                "en",
+            ),
+            "large",
+        )
