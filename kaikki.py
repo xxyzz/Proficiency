d615a622113d3a6144f120f37ba5579323a62a92
xxyzz <gitpull@protonmail.com>
2025-12-02 21:23:53 +0800

    Get kaikki file date from HTTP header

--- kaikki.py
+++ kaikki.py
@@ -1,19 +1,15 @@
 import sys
 
 if __name__ == "__main__":
-    import re
-
     import requests
-    from lxml import etree
+    from datetime import datetime
 
     gloss_lang = sys.argv[1]
     url = (
-        "https://kaikki.org/dictionary/rawdata.html"
+        "https://kaikki.org/dictionary/raw-wiktextract-data.jsonl.gz"
         if gloss_lang in ["en", "he"]
-        else f"https://kaikki.org/{gloss_lang}wiktionary/rawdata.html"
+        else f"https://kaikki.org/{gloss_lang}wiktionary/raw-wiktextract-data.jsonl.gz"
     )
-    r = requests.get(url)
-    root = etree.HTML(r.text)
-    text = root.xpath("body/div[last()]/p[1]/text()")[0]
-    m = re.search(r"(\d{4}-\d{2}-\d{2})", text)
-    print(m.group(1))
+    r = requests.head(url)
+    date = datetime.strptime(r.headers["Last-Modified"], "%a, %d %b %Y %H:%M:%S %Z")
+    print(date.strftime("%Y-%m-%d"))
